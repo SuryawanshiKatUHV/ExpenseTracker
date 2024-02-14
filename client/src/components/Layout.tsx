37db55c9ff4c61b8b154cb94bfade87da9cf12c8
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-02-14 13:06:58 -0600

    Updated the menu items

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 const Layout = ({ children, onSelectItem } : Props) => {
-  const items = ["Dashboard", "Users", "Categories", "Expenses", "Incomes"];
+  const items = ["Dashboard", "Categories", "Transactions", "Groups"];
 
   return (
     <div>
