c76ea65047313aaf4aaaf752126fd0fbf26446a1
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-02-23 12:11:14 -0600

    Added Budget entity template and updated name of the components from transfer to groupTransaction

--- server/server.js
+++ server/server.js
@@ -6,16 +6,18 @@ app.use(express.json());
 // Import routes
 const userRouter = require("./routers/userRouter");
 const categoryRouter = require("./routers/categoryRouter");
+const budgetRouter = require("./routers/budgetRouter");
 const transactionRouter = require("./routers/transactionRouter");
 const groupRouter = require("./routers/groupRouter");
-const transferRouter = require("./routers/transferRouter");
+const groupTransactionRouter = require("./routers/groupTransactionRouter");
 
 // Use routes
 app.use("/api/users", userRouter);
 app.use("/api/categories", categoryRouter);
+app.use("/api/budgets", budgetRouter);
 app.use("/api/transactions", transactionRouter);
 app.use("/api/groups", groupRouter);
-app.use("/api/transfers", transferRouter);
+app.use("/api/groupTransactions", groupTransactionRouter);
 
 // Finally start the server
 const port = process.env.PORT || 3000;
