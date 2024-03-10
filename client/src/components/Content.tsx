d2b21ee172fe11b0e1745b2b0129dcd3d7cadea7
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-10 00:43:20 -0600

    Modified to show each view when navigation from left pane is clicked

--- client/src/components/Content.tsx
+++ client/src/components/Content.tsx
@@ -1,13 +1,38 @@
 import { ReactNode } from "react";
+import { FUNCTIONS } from "../Common";
+import BudgetTable from "./functional/BudgetTable";
+import CategoryTable from "./functional/CatagoryTable";
+import Dashboard from "./functional/Dashboard";
+import GroupTable from "./functional/GroupTable";
+import GroupTransactionTable from "./functional/GroupTransactionTable";
+import TransactionTable from "./functional/TransactionTable";
 
 interface Props {
-  children :ReactNode;
+  userId: number;
+  selectedFunction: string;  
 }
 
-const Content = ({ children }: Props) => {
+const Content = ({ userId, selectedFunction }: Props) => {
+  
+  /**
+   * Operations
+   */
+  const loadContent = () : ReactNode => {
+    switch(selectedFunction) {
+      case FUNCTIONS.Dashboard: return <Dashboard userId={userId}/>;
+      case FUNCTIONS.Categories: return <CategoryTable userId={userId}/>;
+      case FUNCTIONS.Budgets: return <BudgetTable userId={userId}/>;
+      case FUNCTIONS.Transactions: return <TransactionTable userId={userId}/>;
+      case FUNCTIONS.Groups: return <GroupTable userId={userId}/>;
+      case FUNCTIONS.GroupTransactions: return <GroupTransactionTable userId={userId}/>;
+    }
+    
+    return "Clicked unknown item.";
+  }
+
   return (
     <div style={{ flex: 1, padding: '20px' }}>
-      {children || "Default Content"}
+      {loadContent()}
     </div>
   );
 };
