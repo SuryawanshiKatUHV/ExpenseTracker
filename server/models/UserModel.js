cf6cfa31227a3e65f4106726b692d8029873d17d
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-10 00:48:19 -0600

    Corrected dummy data store, added login function and updated create function

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -2,10 +2,7 @@ const Joi = require("joi");
 
 class UserModel {
   dummyData = [
-    { id: 1, fname: "fname1", lname: "lname1" },
-    { id: 2, fname: "fname2", lname: "lname2" },
-    { id: 3, fname: "fname3", lname: "lname3" },
-    { id: 4, fname: "fname4", lname: "lname4" },
+    { id: 1, firstName: "firstName", lastName:"lastName", email:"email@domain.com", password:"password"}
   ];
   idCounter = this.dummyData.length;
 
@@ -21,24 +18,34 @@ class UserModel {
     return user;
   }
 
-  create({ fname, lname }) {
-    this._validate({ fname, lname });
+  create({ email, firstName, lastName, password }) {
+    this._validate({ email, firstName, lastName, password });
 
-    const user = { id: ++this.idCounter, fname: fname, lname: lname };
+    const user = { id: ++this.idCounter, email, firstName, lastName, password };
     this.dummyData.push(user);
     return user;
   }
 
-  update(id, { fname, lname }) {
-    this._validate({ fname, lname });
+  login({ email, password }) {
+    this._validate({ email, password });
+
+    const user = this.dummyData.find((u) => u.email === email && u.password === password);
+    if (!user) {
+      throw new Error(`Authentication failed.`);
+    }
+    return user;
+  }
+
+  update(id, { firstName, lastName }) {
+    this._validate({ firstName, lastName });
 
     const user = this.dummyData.find((u) => u.id === id);
     if (!user) {
       throw { message: `No record found with id ${id}` };
     }
 
-    user.fname = fname;
-    user.lname = lname;
+    user.firstName = firstName;
+    user.lastName = lastName;
 
     return user;
   }
@@ -55,14 +62,16 @@ class UserModel {
 
   _validate(user) {
     const schema = Joi.object({
-      fname: Joi.string().required().min(3),
-      lname: Joi.string().required().min(3),
+      email: Joi.string().required().min(3).max(50),
+      firstName: Joi.string().required().min(3).max(50),
+      lastName: Joi.string().required().min(3).max(50),
+      password: Joi.string().required().min(3).max(50),
     });
 
     const validateResult = schema.validate(user);
     if (validateResult.error) {
       console.log(validateResult.error);
-      throw validateResult.error;
+      throw new Error(validateResult.error);
     }
   }
 }
