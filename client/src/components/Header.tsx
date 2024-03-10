97ec8eba027a104abb321377f7bf6f0a4f08396d
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-10 00:41:29 -0600

    Modified to show the fullname of the logged in user

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,15 @@
-const Header = () => {
+interface Props {
+  userFullName: string;
+  logoutHandler : () => void;
+}
+
+const Header = ({userFullName, logoutHandler} : Props) => {
   return (
-    <div style={{ backgroundColor: '#f0f0f0', padding: '20px', textAlign: 'center' }}>
-      <h1>Expense Tracker</h1>
-    </div>
+    <>
+      <div style={{ backgroundColor: '#f0f0f0', padding: '2px', textAlign: 'right' }}>
+        {userFullName} | <button className="btn btn-link" onClick={logoutHandler}>Logout</button>
+      </div>
+    </>
   );
 };
 
