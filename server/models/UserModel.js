e2c4bebbec4e2e2409a674084edec49152c01f39
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-10 19:48:56 -0500

    All the methods are now implemented to perform database interaction and report appropriate information to client.

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,71 +1,136 @@
 const Joi = require("joi");
+const connectionPool = require('../database');
 
 class UserModel {
-  dummyData = [
-    { id: 1, firstName: "firstName", lastName:"lastName", email:"email@domain.com", password:"password"}
-  ];
-  idCounter = this.dummyData.length;
 
   getAll() {
-    return this.dummyData;
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT USER_ID, USER_EMAIL, USER_FNAME, USER_LNAME FROM USER";
+      connectionPool.query(sql, [], (error, result) => {
+        if (error) {
+          reject (error);
+        }
+        else {
+          resolve(result);
+        }
+      });
+    });
   }
 
   getById(id) {
-    const user = this.dummyData.find((u) => u.id === id);
-    if (!user) {
-      throw { message: `No record found with id ${id}` };
-    }
-    return user;
+    return new Promise((resolve, reject) => {
+      const sql = "SELECT USER_ID, USER_EMAIL, USER_FNAME, USER_LNAME FROM USER WHERE USER_ID=?";
+      connectionPool.query(sql, [id], (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        else {
+          resolve (result);
+        }
+      });
+    });
   }
 
-  create({ email, firstName, lastName, password }) {
-    this._validate({ email, firstName, lastName, password });
+  create(userData) {
+    return new Promise((resolve, reject) => {
+      this._validate(userData);
 
-    const user = { id: ++this.idCounter, email, firstName, lastName, password };
-    this.dummyData.push(user);
-    return user;
+      const sql = "INSERT INTO USER (USER_EMAIL, USER_FNAME, USER_LNAME, USER_PASSWORD) VALUES (?, ?, ?, ?)";
+      connectionPool.query(sql, [userData.USER_EMAIL, userData.USER_FNAME, userData.USER_LNAME, userData.USER_PASSWORD], (error, result) => {
+        if (error) {
+          reject(error);
+        }
+
+        const sql = "SELECT * FROM USER WHERE USER_EMAIL=?";
+        connectionPool.query(sql, [userData.USER_EMAIL], (error, result) => {
+
+          if (error) {
+            reject(error);
+          }
+          else {
+            resolve(result[0]);
+          }
+        });
+      });
+    });
   }
 
-  login({ email, password }) {
-    this._validate({ email, password });
+  login({ USER_EMAIL, USER_PASSWORD }) {
+    return new Promise((resolve, reject)=>{
 
-    const user = this.dummyData.find((u) => u.email === email && u.password === password);
-    if (!user) {
-      throw new Error(`Authentication failed.`);
-    }
-    return user;
+      const sql = "SELECT * FROM USER WHERE LOWER(USER_EMAIL)=LOWER(?) AND USER_PASSWORD=?";
+      connectionPool.query(sql, [USER_EMAIL, USER_PASSWORD], (error, result) => {
+        console.log(`result.length ${result.length}`);
+        if (error) {
+          reject(error);
+        }
+        else if (result.length == 0) {
+          reject(new Error(`Authentication failed. Login denied.`));
+        }
+        else {
+          // For security reason mask the password
+          result[0].USER_PASSWORD = "*****";
+          resolve(result[0]);
+        }
+      });
+    });
   }
 
-  update(id, { firstName, lastName }) {
-    this._validate({ firstName, lastName });
-
-    const user = this.dummyData.find((u) => u.id === id);
-    if (!user) {
-      throw { message: `No record found with id ${id}` };
-    }
-
-    user.firstName = firstName;
-    user.lastName = lastName;
-
-    return user;
+  update(id, { USER_FNAME, USER_LNAME }) {
+    return new Promise((resolve, reject) => {
+      this.getById(id)
+      .then(result => {
+        if(result.length == 0) {
+          throw new Error(`No record found for id ${id}`);
+        }
+        
+        const sql = "UPDATE USER SET USER_FNAME=?, USER_LNAME=? WHERE USER_ID=?";
+        connectionPool.query(sql, [USER_FNAME, USER_LNAME, id], (error, result) => {
+          if (error) {
+            reject(error);
+          }
+          else {
+            resolve(result);
+          }
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+    });
+    
   }
 
   delete(id) {
-    const user = this.dummyData.find((u) => u.id === id);
-    if (!user) {
-      throw { message: `No record found with id ${id}` };
-    }
-    const index = this.dummyData.indexOf(user);
-    this.dummyData.splice(index, 1);
-    return user;
+    return new Promise((resolve, reject) => {
+      this.getById(id)
+      .then(result => {
+        if(result.length == 0) {
+          throw new Error(`No record found for id ${id}`);
+        }
+        
+        const sql = "DELETE FROM USER WHERE USER_ID=?";
+        connectionPool.query(sql, [id], (error, result) => {
+          if (error) {
+            reject(error);
+          }
+          else {
+            resolve(result);
+          }
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+    });
   }
 
   _validate(user) {
     const schema = Joi.object({
-      email: Joi.string().required().min(3).max(50),
-      firstName: Joi.string().required().min(3).max(50),
-      lastName: Joi.string().required().min(3).max(50),
-      password: Joi.string().required().min(3).max(50),
+      USER_EMAIL: Joi.string().required().min(3).max(50),
+      USER_FNAME: Joi.string().required().min(3).max(50),
+      USER_LNAME: Joi.string().required().min(3).max(50),
+      USER_PASSWORD: Joi.string().required().min(3).max(50),
     });
 
     const validateResult = schema.validate(user);
