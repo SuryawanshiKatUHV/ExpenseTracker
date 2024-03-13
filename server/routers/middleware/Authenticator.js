3e572c10e8f572fde176bb4afc0e6487b527bc0d
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-13 18:00:42 -0500

    Middleware is added to authenticate and protect the webservice endpoints

---
+++ server/routers/middleware/Authenticator.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate the token
+const authenticateToken = (req, res, next) => {
+  console.log(`Invoked authenticateToken...`);
+  // Retrieve the token from the Authorization header
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN_VALUE
+
+  console.log(`req.headers=${JSON.stringify(req.headers)}`);
+  console.log(`token=${token}`);
+
+  if (token == null) {
+    return res.sendStatus(401); // If no token, return Unauthorized
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // If token is not valid, return Forbidden
+    }
+    req.user = user; // Set the user in the request object
+    next(); // Proceed to the next middleware or request handler
+  });
+};
+
+module.exports = authenticateToken;
