68e941d004914664ca5b4de1f90fa803d0de077c
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-16 15:00:26 -0500

    Corrected create function and implemented getAll and getById

--- server/controllers/CategoryController.js
+++ server/controllers/CategoryController.js
@@ -7,7 +7,10 @@ class CategoryController {
   async getAll(req, res) {
     console.log("Category getAll is invoked...");
     try {
-      const data = await categoryModel.getAll(); // Correctly await the promise
+      const user = req.user;
+
+      console.log(`Received req.user=${JSON.stringify(req.user)}`);
+      const data = await categoryModel.getAll(user.USER_ID); // Correctly await the promise
 
       res.status(200).json(data); // Use 200 status code and ensure to send the awaited data
     } catch (error) {
@@ -18,27 +21,34 @@ class CategoryController {
 
 
   async getById(req, res) {
+    const user = req.user;
     const { id } = req.params;
     try {
-      res.status(201).json(catgeoryModel.getById(parseInt(id))); // Send the retrieved object as a JSON response
+      const data = await categoryModel.getById(user.USER_ID, parseInt(id));
+      console.log(`data=${JSON.stringify(data)}`);
+
+      res.status(201).json(data); // Send the retrieved object as a JSON response
     } catch (error) {
+      console.error(error.message);
       res.status(500).json({ message: error.message });
     }
   }
 
   async create(req, res) {
     console.log("Category create is invoked...");
+    try {
+      const categoryData = req.body;
+      console.log(`categoryData=${JSON.stringify(categoryData)}`);
 
-    const categoryData = req.body;
-    const data = await CategoryModel.create(categoryData);
+      const data = await CategoryModel.create(categoryData);
 
-    res.status(201).json(data); // Send the saved object as a JSON response
-      console.log(`Category is created ${data}`);
+      res.status(201).json(data); // Send the saved object as a JSON response
+      console.log(`Category is created ${JSON.stringify(data)}`);
     } catch (error) {
       console.error(error.message);
       res.status(500).json({ message: error.message });
     }
-
+  }
 
   async update(req, res) {
     const { id } = req.params;
