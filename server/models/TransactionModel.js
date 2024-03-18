d868fa650eb60aa6ed7ee17715f5a02fc67c4550
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-17 20:36:00 -0500

    validate and create function is implemented

--- server/models/TransactionModel.js
+++ server/models/TransactionModel.js
@@ -1,4 +1,5 @@
 const Joi = require("joi");
+const connectionPool = require("../database");
 
 class TransactionModel {
   dummyData = [
@@ -13,8 +14,33 @@ class TransactionModel {
     throw { message: `To be implemented` };
   }
 
-  create({ key1, key2 }) {
-    throw { message: `To be implemented` };
+  // CATEGORY_ID INT NOT NULL,
+  // TRANSACTION_TYPE ENUM('Expense', 'Income') NOT NULL,
+  // TRANSACTION_DATE DATETIME NOT NULL,
+  // TRANSACTION_AMOUNT DECIMAL(10,2) DEFAULT 0.00,
+  // TRANSACTION_NOTES VARCHAR(100),
+  create(transactionData) {
+    return new Promise((resolve, reject) => {
+      this._validate(transactionData);
+
+      const sql = "INSERT INTO TRANSACTION (CATEGORY_ID, TRANSACTION_TYPE, TRANSACTION_DATE, TRANSACTION_AMOUNT, TRANSACTION_NOTES) VALUES (?, ?, ?, ?, ?)";
+      connectionPool.query(sql, [transactionData.CATEGORY_ID, transactionData.TRANSACTION_TYPE, transactionData.TRANSACTION_DATE, transactionData.TRANSACTION_AMOUNT, transactionData.TRANSACTION_NOTES], (error, result) => {
+        if (error) {
+          reject(error);
+        }
+
+        const sql = "SELECT * FROM TRANSACTION WHERE CATEGORY_ID=? AND TRANSACTION_TYPE=? AND TRANSACTION_DATE=? AND TRANSACTION_AMOUNT=? AND TRANSACTION_NOTES=?";
+        connectionPool.query(sql, [transactionData.CATEGORY_ID, transactionData.TRANSACTION_TYPE, transactionData.TRANSACTION_DATE, transactionData.TRANSACTION_AMOUNT, transactionData.TRANSACTION_NOTES], (error, result) => {
+
+          if (error) {
+            reject(error);
+          }
+          else {
+            resolve(result[0]);
+          }
+        });
+      });
+    });
   }
 
   update(id, { key1, key2 }) {
@@ -25,13 +51,21 @@ class TransactionModel {
     throw { message: `To be implemented` };
   }
 
-  _validate(user) {
+  // CATEGORY_ID INT NOT NULL,
+  // TRANSACTION_TYPE ENUM('Expense', 'Income') NOT NULL,
+  // TRANSACTION_DATE DATETIME NOT NULL,
+  // TRANSACTION_AMOUNT DECIMAL(10,2) DEFAULT 0.00,
+  // TRANSACTION_NOTES VARCHAR(100) NOT NULL,
+  _validate(transactionData) {
     const schema = Joi.object({
-      key1: Joi.string().required().min(3),
-      key2: Joi.string().required().min(3),
+      CATEGORY_ID: Joi.number().required(),
+      TRANSACTION_TYPE: Joi.string().required(), //TODO must be from Expense/Income
+      TRANSACTION_DATE: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
+      TRANSACTION_AMOUNT: Joi.number().required(),//Not negative
+      TRANSACTION_NOTES: Joi.string().required()
     });
 
-    const validateResult = schema.validate(user);
+    const validateResult = schema.validate(transactionData);
     if (validateResult.error) {
       console.log(validateResult.error);
       throw validateResult.error;
