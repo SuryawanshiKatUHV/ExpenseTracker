36f002a88ad1144efd2475fccae17a72a0923f15
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-17 20:34:29 -0500

    Not implemented functions are now blanked out, create function is implemented

--- server/controllers/TransactionController.js
+++ server/controllers/TransactionController.js
@@ -3,48 +3,33 @@ const transactionModel = require("../models/TransactionModel");
 class TransactionController {
 
   async getAll(req, res) {
-    try {
-      res.json(transactionModel.getAll()); // Send the categories as a JSON response
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    throw { message: `To be implemented` };
   }
 
   async getById(req, res) {
-    const { id } = req.params;
-    try {
-      res.status(201).json(transactionModel.getById(parseInt(id))); // Send the retrieved object as a JSON response
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    throw { message: `To be implemented` };
   }
 
   async create(req, res) {
-    const { key1, key2 } = req.body;
+    console.log("Transaction create is invoked...");
     try {
-      res.status(201).json(transactionModel.create({key1, key2})); // Send the saved object as a JSON response
+      const transactionData = req.body;
+      const data = await transactionModel.create(transactionData);
+
+      res.status(201).json(data); // Send the saved object as a JSON response
+      console.log(`Transaction is created ${data}`);
     } catch (error) {
+      console.error(error.message);
       res.status(500).json({ message: error.message });
     }
   }
 
   async update(req, res) {
-    const { id } = req.params;
-    const { key1, key2 } = req.body;
-    try {
-      res.status(201).json(transactionModel.update(parseInt(id), { key1, key2 })); // Send the saved object as a JSON response
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    throw { message: `To be implemented` };
   }
 
   async delete(req, res) {
-    const { id } = req.params;
-    try {
-      res.status(201).json(transactionModel.delete(parseInt(id))); // Send the deleted object as a JSON response
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+    throw { message: `To be implemented` };
   }
   
 }
