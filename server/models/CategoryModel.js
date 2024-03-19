f559402b93954d4e86902bba16189f2bf60576b6
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-18 19:26:06 -0500

    Throwing Error for the functions yet to be implemented. Reimplemented getAll, getById, create functions using cleaner code.

--- server/models/CategoryModel.js
+++ server/models/CategoryModel.js
@@ -1,86 +1,62 @@
 const Joi = require("joi");
-const connectionPool = require('../database');
+const getConnection = require('../database');
 
 class CategoryModel {
 
-
-  getAll(ownerId) {
-    console.log(`CategoryModel.getAll(${ownerId})`);
-
-    return new Promise((resolve, reject) => {
-      const sql = "SELECT * FROM CATEGORY WHERE OWNER_ID=?";
-      connectionPool.query(sql, [ownerId], (error, result) => {
-        console.log(`error=${error}`);
-        console.log(`result=${result}`);
-        if (error) {
-          reject (error);
-        }
-        else {
-          resolve(result);
-        }
-      });
-    });
+  async getAll(ownerId) {
+    const connection = await getConnection();
+    try {
+      const [rows, fields] = await connection.execute("SELECT * FROM CATEGORY WHERE OWNER_ID=?", [ownerId]);
+      return rows;
+    }
+    finally {
+      connection.release();
+    }
   }
 
-  getById(ownerId, categoryId) {
-    console.log(`CategoryModel.getById(${ownerId}, ${categoryId})`);
+  async getById(ownerId, categoryId) {
+    const connection = await getConnection();
+    try {
+      const [rows, fields] = await connection.execute("SELECT * FROM CATEGORY WHERE OWNER_ID=? AND CATEGORY_ID=?", [ownerId, categoryId]);
+      if (!rows || rows.length == 0) {
+        throw new Error(`No category found for id ${categoryId}`);
+      }
 
-    return new Promise((resolve, reject) => {
-      const sql = "SELECT * FROM CATEGORY WHERE OWNER_ID=? AND CATEGORY_ID=?";
-      connectionPool.query(sql, [ownerId, categoryId], (error, result) => {
-        console.log(`error=${error}`);
-        console.log(`result=${result}`);
-        if (error) {
-          reject (error);
-        }
-        else {
-          resolve(result);
-        }
-      });
-    });
+      return rows[0];
+    }
+    finally {
+      connection.release();
+    }
   }
 
-  create(categoryData) {
-      return new Promise((resolve, reject) => {
-          // Assuming there's a simple validation function for category data
-          this._validate(categoryData);
-  
-          const sql = "INSERT INTO CATEGORY (OWNER_ID, CATEGORY_TITLE, CATEGORY_DESCRIPTION) VALUES (?, ?, ?)";
-          connectionPool.query(sql, [categoryData.OWNER_ID, categoryData.CATEGORY_TITLE, categoryData.CATEGORY_DESCRIPTION], (error, result) => {
-            console.log(`error=${error}`);
-            console.log(`result=${result}`);
+  async create({OWNER_ID, CATEGORY_TITLE, CATEGORY_DESCRIPTION}) {
+    const connection = await getConnection();
+    try {
+      this._validate({OWNER_ID, CATEGORY_TITLE, CATEGORY_DESCRIPTION});
 
-              if (error) {
-                  reject(error);
-              } 
-              else {    
-                  const sql = "SELECT * FROM CATEGORY WHERE OWNER_ID = ? AND CATEGORY_TITLE=?";
-                  connectionPool.query(sql, [categoryData.OWNER_ID, categoryData.CATEGORY_TITLE], (error, result) => {
-                      if (error) {
-                          reject(error);
-                      } else {
-                          resolve(result[0]); 
-                      }
-                  });
-                }
-          });
-      });
-  
+      const result = await connection.execute("INSERT INTO CATEGORY (OWNER_ID, CATEGORY_TITLE, CATEGORY_DESCRIPTION) VALUES (?, ?, ?)", [OWNER_ID, CATEGORY_TITLE, CATEGORY_DESCRIPTION]);
+      console.log(`Category inserted with id ${result[0].insertId}`);
+
+      return {CATEGORY_ID:result[0].insertId};
+    }
+    finally {
+      connection.release();
+    }
   }
 
-  update(id, { key1, key2 }) {
-    throw { message: `To be implemented` };
+  async update(id, { key1, key2 }) {
+    throw new Error(`To be implemented`);
   }
 
-  delete(id) {
-    throw { message: `To be implemented` };
+  async delete(id) {
+    throw new Error(`To be implemented`);
   }
 
   _validate(user) {
     const schema = Joi.object({
       OWNER_ID: Joi.number().required(),
       CATEGORY_TITLE: Joi.string().required().min(3).max(50),
-      CATEGORY_DESCRIPTION: Joi.string().required().min(3).max(50),
+      CATEGORY_DESCRIPTION: Joi.string().required().min(3).max(100),
     });
 
     const validateResult = schema.validate(user);
