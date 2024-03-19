75f4bc67b34a68acb1ec8054ea1e1cb9f4ac3b14
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-18 19:29:42 -0500

    Reimplemented using mysql2/promise model, which would provide cleaner code in all the models

--- server/database.js
+++ server/database.js
@@ -1,12 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Initialize MySQL connection pool
 const connectionPool = mysql.createPool({
-    connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root123',
-    database: process.env.DB_NAME || 'expense_tracker'
-  });
-  
-  module.exports = connectionPool;
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || 10,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root123',
+  database: process.env.DB_NAME || 'expense_tracker',
+});
+
+// Export a function that returns a Promise that resolves to a connection from the pool
+module.exports = async function() {
+  const connection = await connectionPool.getConnection();
+  return connection;
+};
