cde1f8fa53c35b0f61d3b0714b5c887c146e21c7
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-22 16:32:42 -0500

    This file is renamed. Then this file is moved to the top level middleware folder.

---
+++ server/middleware/authenticator.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware to authenticate the token
+const authenticator = (req, res, next) => {
+  console.log(`Invoked authenticateToken...`);
+  // Retrieve the token from the Authorization header
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN_VALUE
+
+  console.log(`req.headers=${JSON.stringify(req.headers)}`);
+  console.log(`token=${token}`);
+
+  if (token == null) {
+    return res.sendStatus(401); // If no token, return Unauthorized
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.sendStatus(403); // If token is not valid, return Forbidden
+    }
+
+    req.user = user; // Set the user in the request object
+    console.log(`req.user=${JSON.stringify(req.user)}`);
+    next(); // Proceed to the next middleware or request handler
+  });
+};
+
+module.exports = authenticator;
