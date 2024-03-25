26b0c2748f65835f5560927d89b586ff0ebb3728
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-25 18:03:56 -0500

    Added getMembers, getTransactions, getSettlementSummary methods.

--- server/routers/groupRouter.js
+++ server/routers/groupRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const authenticator = require('../middleware/Authenticator');
 const controller = require("../controllers/GroupController");
 
+/**
+ * Basic services
+ */
 router.get("/", authenticator, controller.getAll);
 
 router.get("/:id", authenticator, controller.getById);
@@ -12,5 +15,14 @@ router.post("/", authenticator, controller.create);
 router.put("/:id", authenticator, controller.update);
 
 router.delete("/:id", authenticator, controller.delete);
+
+/**
+ * Extended services
+ */
+router.get("/:id/members", authenticator, controller.getMembers);
+
+router.get("/:id/transactions", authenticator, controller.getTransactions);
+
+router.get("/:id/settlementSummary", authenticator, controller.getSettlementSummary);
 
 module.exports = router;
