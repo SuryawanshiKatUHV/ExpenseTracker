2ea6b6ff881520d727233b34d58e934307e1a426
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-03-24 22:19:17 -0500

    Implemented error handling. Moved save logic from table to here. Removed unused state categories.

--- client/src/components/functional/CategoryForm.tsx
+++ client/src/components/functional/CategoryForm.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
-import { END_POINTS, get } from "../../Common";
+import { useState } from "react";
+import { END_POINTS, post } from "../../Common";
 
 interface Props {
     userId: number;
-    saveHandler: (categoryTitle: string, categoryDescription:string) => void;
+    saveHandler: () => void;
     cancelHandler: () => void;
 }
 
@@ -12,31 +12,45 @@ const CategoryForm = (props : Props) => {
     // State for category input form
     const [categoryTitle, setCategoryTitle] = useState('');
     const [categoryDescription, setCategoryDescription] = useState('');
-    const [categories, setCategories] = useState<any>([]);
-    const [errors, setErrors] = useState({categoryTitle: '', categoryDescription: ''});
-    const [showForm, setShowForm] = useState(false);
+    const [validationErrors, setValidationErrors] = useState({categoryTitle: '', categoryDescription: ''});
+    const [error, setError] = useState('');
 
     // Validate category input
     const validateInput = () => {
         let isValid = true;
-        let errors = {categoryTitle: '', categoryDescription: ''};
+        let validationErrors = {categoryTitle: '', categoryDescription: ''};
 
         if (!categoryTitle) {
-            errors.categoryTitle = 'Category title is required.';
+            validationErrors.categoryTitle = 'Category title is required.';
             isValid = false;
         }
         if (!categoryDescription) {
-            errors.categoryDescription = 'Category description is required.';
+            validationErrors.categoryDescription = 'Category description is required.';
             isValid = false;
         }
 
-        setErrors(errors);
+        setValidationErrors(validationErrors);
         return isValid;
     };
 
-    const SaveClicked = () => {
+    const SaveClicked = async () => {
         if(validateInput()) {
-            props.saveHandler(categoryTitle, categoryDescription);
+
+            const categoryData = {
+                OWNER_ID: props.userId,
+                CATEGORY_TITLE: categoryTitle,
+                CATEGORY_DESCRIPTION: categoryDescription
+            };
+    
+            try {
+                const result = await post(END_POINTS.Categories, categoryData);
+                console.log(`Category created with id ${result.CATEGORY_ID}`);
+
+                props.saveHandler();
+            } 
+            catch (error : any) {
+                setError(error.message);
+            }
         }
     }
 
@@ -44,39 +58,31 @@ const CategoryForm = (props : Props) => {
         props.cancelHandler();
     }
 
-
-
     return (
         
         <div style={{border:1}}>
-            <h5 className="m-5">Add new group transaction</h5>
+            <h5 className="m-5">Add new category</h5>
 
-            {/* <div className="form-floating mb-3">
-              <input type="number" className="form-control" id="UserId" value={userId} onChange={(e) => setUserId(e.target.value)}/>
-              <label htmlFor="UserId">Owner Id</label>
-              {errors.userId && <p style={{color:'red'}}>{errors.userId}</p>}
-            </div> */}
-            
             <div className="form-floating mb-3">
               <input type="string" className="form-control" id="categoryTitle" value={categoryTitle} onChange={(e) => setCategoryTitle(e.target.value)}/>
               <label htmlFor="categoryTitle">Category Title</label>
-              {errors.categoryTitle && <p style={{color:'red'}}>{errors.categoryTitle}</p>}
+              {validationErrors.categoryTitle && <p style={{color:'red'}}>{validationErrors.categoryTitle}</p>}
             </div>
     
             <div className="form-floating mb-3">
               <input type="string" className="form-control" id="categoryDescription" value={categoryDescription} onChange={(e) => setCategoryDescription(e.target.value)}/>
               <label htmlFor="categoryDescription">Category Description</label>
-              {errors.categoryDescription && <p style={{color:'red'}}>{errors.categoryDescription}</p>}
+              {validationErrors.categoryDescription && <p style={{color:'red'}}>{validationErrors.categoryDescription}</p>}
             </div>
-    
-
     
             <div>
                 <button className="btn btn-success" onClick={SaveClicked}>Save</button> &nbsp; 
                 <button className="btn btn-danger" onClick={CancelClicked}>Cancel</button>
             </div>
+
+            {error && <p style={{color:'red'}}>{error}</p>}
         </div>
-        );
+    );
 };
 
 
