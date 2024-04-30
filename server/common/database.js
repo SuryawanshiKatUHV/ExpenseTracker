9c1d2d030e75e8d84c93a0bccab52bca27911995
SuryawanshiKatUHV <SuryawanshiK@uhv.edu>
2024-04-30 08:53:46 -0500

    Removed executeUsingConnection() as it is not adding any value

--- server/common/database.js
+++ server/common/database.js
@@ -28,23 +28,6 @@ async function execute(sql, params) {
     connection.release();
   }
 }
-/**
- * Executes the given prepared SQL with given connection object. 
- * This function is useful when the connection's transaction is maintained by client code.
- * 
- * @param {mysql connection} connection The database connection object
- * @param {string} sql Prepared SQL statement
- * @param {string[]} params The parameters to the prepared SQL statement
- * @returns Result of the SQL execution
- */
-async function executeUsingConnection(connection, sql, params) {
-  try {
-    return await connection.execute(sql, params);
-  } catch (error) {
-    console.error(`Database error: ${error.message}`);
-    throw error;
-  }
-}
 
 /**
  * Gets the datbase connection from the pool
@@ -57,4 +40,4 @@ async function getConnection() {
 }
 
 // Export a function that returns a Promise that resolves to a connection from the pool
-module.exports = {execute, executeUsingConnection, getConnection};
+module.exports = {execute, getConnection};
